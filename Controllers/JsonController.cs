3e155ee4d5d52b50ecb99dc654eb3b6e6355ad23
BhaaL <bhaal@0x1337.org>
2018-12-01 14:27:52 +0100

    Add project files.

---
+++ Controllers/JsonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestNexus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JsonController : ControllerBase
+    {
+        [HttpGet("{*url}")]
+        public ActionResult<object> Get(string url)
+        {
+            return NotFound();
+        }
+
+        [HttpPost("{*url}")]
+        public ActionResult<object> Post(string url, [FromBody] object body)
+        {
+            return NotFound();
+        }
+
+        [HttpPut("{*url}")]
+        public ActionResult<object> Put(string url, [FromBody] object body)
+        {
+            return NotFound();
+        }
+    }
+}
