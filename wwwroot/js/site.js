25f446b68bb43a533dd0ed0e18371a668d459b67
BhaaL <bhaal@0x1337.org>
2019-02-24 15:31:03 +0100

    generate a full url on the list and detail pages

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,15 @@
-﻿// Write your Javascript code.
+﻿function prettyFullUrl(baseUrl, urlTemplate) {
+    var cleanUrl = urlTemplate.replace(/^\/*/g, '').replace(/\/+/g, '/');
+
+    var segments = cleanUrl
+        .split('/')
+        .map(function (s) {
+            if (s.startsWith(':'))
+                return '<i>' + s.substring(1) + '</i>';
+            return s;
+        });
+    segments.unshift(baseUrl);
+
+    var fullUrl = segments.join('/');
+    return fullUrl;
+}
